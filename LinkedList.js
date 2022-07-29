84bb3a6d1adc6a4bfb365849c6e17fa1cb9e66d0
Julio <juliornellas@gmail.com>
2022-07-29 15:25:25 -0300

    Test

--- LinkedList.js
+++ LinkedList.js
@@ -1,7 +1,20 @@
 class LinkedList{
     constructor(){
-        this.head
-        this.length
+        this.head = null
+        this.length = 0
+    }
+
+    insertAtHead(data){
+        const newNode = new LinkedListNode(data, this.head)
+        this.head = newNode
+        this.length++
+    }
+}
+
+class LinkedListNode {
+    constructor(value, next){
+        this.value = value
+        this.next = next
     }
 }
 
---
+++ app.js
@@ -0,0 +1,9 @@
+const LinkedList = require('./LinkedList');
+
+const ll = new LinkedList();
+ll.insertAtHead(10)
+ll.insertAtHead(20)
+ll.insertAtHead(30)
+ll.insertAtHead(40)
+
+console.log(ll)
